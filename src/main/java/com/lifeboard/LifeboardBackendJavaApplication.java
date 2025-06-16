62b83f75a3ab21f4c1987bcf95e0c0d9bf71fdf0
felipesora <felipeusora@gmail.com>
2025-06-16 14:30:41 -0300

    first commit

---
+++ src/main/java/com/lifeboard/LifeboardBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lifeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeboardBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeboardBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifeboard/LifeboardBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeboardBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
